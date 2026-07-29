7754afe2647a98a64459e24227036635c2796578
jjwwhh0416 <jjwwhh0416@gmail.com>
2026-07-29 14:51:12 +0900

    sdf

--- data_process/CLIP_latent.py
+++ data_process/CLIP_latent.py
@@ -20,30 +20,39 @@ processor = CLIPProcessor.from_pretrained(
 
 image_dir = Path("/mnt/ddn/prod-shared/datasets/DriveLM/nuscenes/samples/CAM_FRONT")
 
-# jpg 정렬
-images = sorted(image_dir.glob("*.jpg"))
+# 사용할 scene
+scene_prefix = "n015-2018-07-24-10-42-41+0800"
 
-# nuScenes는 2FPS → 1FPS
+# 해당 scene의 이미지만 선택
+images = sorted(image_dir.glob(f"{scene_prefix}*.jpg"))
+
+print(f"Total images: {len(images)}")
+
+# nuScenes는 2 FPS → Vid2Seq는 1 FPS
 images = images[::2]
+
+print(f"Using images: {len(images)}")
 
 features = []
 
 with torch.no_grad():
     for img_path in tqdm(images):
-
         image = Image.open(img_path).convert("RGB")
 
-        inputs = processor(images=image, return_tensors="pt").to(device)
+        inputs = processor(
+            images=image,
+            return_tensors="pt"
+        ).to(device)
 
         feat = model.get_image_features(**inputs)
 
-        # L2 normalize (CLIP 기본 사용법)
+        # CLIP feature normalization
         feat = feat / feat.norm(dim=-1, keepdim=True)
 
         features.append(feat.cpu().numpy()[0])
 
 features = np.stack(features)
 
-print(features.shape)
+print("Feature shape:", features.shape)
 
 np.save("scene001.npy", features)
